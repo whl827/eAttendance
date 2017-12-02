86c650fd846339c99ee8b0d9451e59ba551ace75
Student <student@OHE412-17.local>
2017-12-02 12:26:18 -0800

    Initial Commit

---
+++ eAttendance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eAttendance
+//
+//  Created by Student on 12/2/17.
+//  Copyright © 2017 Woonghee Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eAttendance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eAttendance
+//
+//  Created by Student on 12/2/17.
+//  Copyright © 2017 Woonghee Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
